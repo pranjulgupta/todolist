a5e8ed51d70070631c4345411e442f0c6b211577
pranjal gupta <pranjalgupta124@gmail.com>
2019-07-02 14:20:00 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -1,70 +1,60 @@
-const express=require('express');
-const bodyparser=require('body-parser');
-const cors=require('cors');
-const path=require('path')
-const app=express();
+const express = require('express');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+const app = express();
 var db;
 var MongoClient = require('mongodb').MongoClient
-, assert = require('assert'),
- ObjectID = require('mongodb').ObjectID
+    , assert = require('assert'),
+    ObjectID = require('mongodb').ObjectID
 var url = 'mongodb://localhost:27017';
-app.use(express.static(__dirname+'/dist/angular6'));
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname+ '/dist/angular6/index.html'));
-  });
+MongoClient.connect(url, function (err, client) {
+    assert.equal(null, err);
 
-
-MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-
-db=client.db("todo");
+    db = client.db("todo");
 })
 app.use(cors());
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: true }))
 
 
-app.post("/home",function(req,res)
-{ let tasks=req.body;  
-  
-    db.collection('list1').insertOne(tasks, function(err, r) {
-       res.json(r);
-      
+app.post("/home", function (req, res) {
+    let tasks = req.body;
+
+    db.collection('list1').insertOne(tasks, function (err, r) {
+        res.json(r);
+
     })
-    
+
 })
-app.post('/home1',function(req,res)
-{
-    db.collection('list1').find({}).toArray(function(err,result){
-       res.json(result)
-     })
+app.post('/home1', function (req, res) {
+    db.collection('list1').find({}).toArray(function (err, result) {
+        res.json(result)
+    })
 })
-app.post('/home2',function(req,res)
-{let tasks1=req.body;
-    db.collection('list1').deleteOne(tasks1, function(err, r) {
+app.post('/home2', function (req, res) {
+    let tasks1 = req.body;
+    db.collection('list1').deleteOne(tasks1, function (err, r) {
         res.json(r);
     })
 })
-app.post('/home3',function(req,res)
-{let name=req.body.name;
-    let priority=req.body.priority;
-let id1=req.body.id;
-console.log(id1);
-    db.collection('list1').updateOne({'_id': ObjectID(id1)},{$set:{'name':name,'priority':priority}}, function(err, r) {
+app.post('/home3', function (req, res) {
+    let name = req.body.name;
+    let priority = req.body.priority;
+    let id1 = req.body.id;
+    console.log(id1);
+    db.collection('list1').updateOne({ '_id': ObjectID(id1) }, { $set: { 'name': name, 'priority': priority } }, function (err, r) {
         res.json(r);
-        })
+    })
 })
 
-app.post('/home4',function(req,res)
-{let status1=req.body.status;
-let id1=req.body.id;
-    db.collection('list1').updateOne({'_id': ObjectID(id1)},{$set:{'status':status1}}, function(err, r) {
+app.post('/home4', function (req, res) {
+    let status1 = req.body.status;
+    let id1 = req.body.id;
+    db.collection('list1').updateOne({ '_id': ObjectID(id1) }, { $set: { 'status': status1 } }, function (err, r) {
         res.json(r);
-        })
+    })
 })
-app.listen(8080,function()
-{
+app.listen(8080, function () {
 
     console.log("server Start")
 })
-
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { $ } from 'protractor';
-import {HttpClient} from '@angular/common/http'
+import { HttpClient } from '@angular/common/http'
 import { ObjectUnsubscribedError } from 'rxjs';
 @Component({
   selector: 'app-list',
@@ -8,107 +8,81 @@ import { ObjectUnsubscribedError } from 'rxjs';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-tasks=[];
-priorities=["low","medium","high"];
- c1=false;
- mode=true;
- today=Date.now();
- selectindex;
-newTask:string;
-newpriority:string;
-url="/home";
-url1="/home1";
-url2="/home2";
-url3="/home3";
-url4="/home4";
+  tasks = [];
+  newtaski;
+  priorities = ["low", "medium", "high"];
+  c1 = false;
+  mode = true;
+  today = Date.now();
+  selectindex;
+  newTask: string;
+  newpriority: string;
+  url = "http://localhost:8080/home";
+  url1 = "http://localhost:8080/home1";
+  url2 = "http://localhost:8080/home2";
+  url3 = "http://localhost:8080/home3";
+  url4 = "http://localhost:8080/home4";
 
-  constructor(private _http:HttpClient) { }
+  constructor(private _http: HttpClient) { }
 
-  add()
-{
+  add() {
 
-this._http.post(this.url,{name: this.newTask,status:false,priority: this.newpriority}).subscribe((data)=>
-{
-  
-});
-this.tasks.push({name: this.newTask,status:false,priority: this.newpriority});
+    this._http.post(this.url, { name: this.newTask, status: false, priority: this.newpriority }).subscribe((data) => {
 
-this.newTask="";
-this.newpriority="";
-}
-remove(index)
- {
-this._http.post(this.url2,{name:this.tasks[index].name,priority:this.tasks[index].priority}).subscribe((data)=>
-{
-  console.log(data);
-});
-this.tasks.splice(index,1);
-}
-newtaski;
-Edit(index)
-{this.newtaski=this.tasks[index].name;
-this.newTask=this.tasks[index].name;
-this.newpriority=this.tasks[index].priority;
-this.mode=false;
-this.selectindex=index;
-event.stopPropagation();
-}
-update()
-{
-this.tasks[this.selectindex].name=this.newTask;
-this.tasks[this.selectindex].priority=this.newpriority;
-console.log(this.tasks[this.selectindex]._id);
-this._http.post(this.url3,{id:this.tasks[this.selectindex]._id,name: this.newTask,priority: this.newpriority}).subscribe((data)=>
-{
+    });
+    this.tasks.push({ name: this.newTask, status: false, priority: this.newpriority });
 
-});
-this.newTask="";
-this.newpriority="";
-this.mode=true;
-}
-toggle(index)
-{
-  this.tasks[index].status=!this.tasks[index].status;
-  this._http.post(this.url4,{id:this.tasks[index]._id,status:this.tasks[index].status}).subscribe((data)=>
-  {
-  
-  });
-}
-panding()
-{var count:number=0;
-  for(let i of this.tasks)
-  {
-    if(i.status==false)
-    count++
+    this.newTask = "";
+    this.newpriority = "";
   }
-  return count;
-}
-taskid=[];
-ngOnInit() {
-  
-  this._http.post(this.url1,{}).subscribe((tasks1:any)=>
-{
-  this.tasks=tasks1;
+  remove(index) {
+    this._http.post(this.url2, { name: this.tasks[index].name, priority: this.tasks[index].priority }).subscribe((data) => {
+      console.log(data);
+    });
+    this.tasks.splice(index, 1);
+  }
+  Edit(index) {
+  this.newtaski = this.tasks[index].name;
+    this.newTask = this.tasks[index].name;
+    this.newpriority = this.tasks[index].priority;
+    this.mode = false;
+    this.selectindex = index;
+    event.stopPropagation();
+  }
+  update() {
+    this.tasks[this.selectindex].name = this.newTask;
+    this.tasks[this.selectindex].priority = this.newpriority;
+    console.log(this.tasks[this.selectindex]._id);
+    this._http.post(this.url3, { id: this.tasks[this.selectindex]._id, name: this.newTask, priority: this.newpriority }).subscribe((data) => {
 
-})
-}
-}
-/*
-    note ---- {title, note}
-*/
+    });
+    this.newTask = "";
+    this.newpriority = "";
+    this.mode = true;
+  }
+  toggle(index) {
+    this.tasks[index].status = !this.tasks[index].status;
+    this._http.post(this.url4, { id: this.tasks[index]._id, status: this.tasks[index].status }).subscribe((data) => {
 
+    });
+  }
+  panding() {
+    var count: number = 0;
+    for (let i of this.tasks) {
+      if (i.status == false)
+        count++
+    }
+    return count;
+  }
+  taskid = [];
+  ngOnInit() {
 
-// addNote(note) {
-//     n.getItem('cards').then(data => {
-//         let newArray = [];
-//         newArray.push(note);
-//         let newData = data.concat(newArray);
-//         n.setItem('cards',newData).then(
-//             () => console.log('Stored item!'),
-//             error => console.error('Error storing item', error)
-//     );
-//     }, err => {
-//     });
-// } 
+    this._http.post(this.url1, {}).subscribe((tasks1: any) => {
+      this.tasks = tasks1;
+
+    })
+  }
+}
+
 
 
